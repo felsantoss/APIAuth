0e5e3d51031a139658e878498502f16051d3d6bc
felsantoss <felipesantossf@outlook.com>
2024-02-25 09:42:00 -0300

    Adicionando Swagger ao projeto

--- Program.cs
+++ Program.cs
@@ -1,5 +1,22 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {Title = "API Auth", Description = "Create a API with JWT", Version = "v1"});
+});
+
 var app = builder.Build();
+
+// Using swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Auth V1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
